1d7557a21b9efb3f15b780bcb78448a959e07045
MarcusDLG <delag010@gmail.com>
2020-11-30 19:44:55 -0500

    testing marcus pull request

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -3,6 +3,11 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, Team!</div>
+    <>
+      <div className="d-flex justify-content-center display-2">
+        Hello, Team!
+      </div>
+      <div>hello bitches</div>
+    </>
   )
 }
